dd46c248ac31c37a23698defa5e369721c6a8ede
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2023-02-02 21:33:16 +0000

    remove defaults and add starter template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { ReactDOM } from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Square extends React.Component {
+    render() {
+        return <button className="square">{this.props.value}</button>;
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    renderRow(i1, i2, i3) {
+        return (
+            <div className="board-row">
+                {this.renderSquare(i1)}
+                {this.renderSquare(i2)}
+                {this.renderSquare(i3)}
+            </div>
+        );
+    }
+
+    render() {
+        const status = "Next Player: X";
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                {this.renderRow(1, 2, 3)}
+                {this.renderRow(4, 5, 6)}
+                {this.renderRow(7, 8, 9)}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div></div>
+                    <ol></ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
