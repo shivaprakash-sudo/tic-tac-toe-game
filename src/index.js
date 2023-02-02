a2f922a1ccbd9408b01954a98f7ed866ecb4dd42
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2023-02-02 22:52:19 +0000

    update board's state declaration

--- src/index.js
+++ src/index.js
@@ -4,13 +4,35 @@ import "../src/App.css";
 
 class Square extends React.Component {
     render() {
-        return <button className="square">{this.props.value}</button>;
+        return (
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
+            </button>
+        );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = [...this.state.squares];
+        squares[i] = "X";
+        this.setState({ squares: squares });
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     renderRow(i1, i2, i3) {
