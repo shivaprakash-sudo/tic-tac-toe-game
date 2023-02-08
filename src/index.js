fa34d5621f9be523430cc23297af943d6a464233
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2023-02-08 22:49:15 +0000

    add logic to calculate and display winner

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = [...this.state.squares];
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? "X" : "O";
         this.setState({
             squares: squares,
@@ -48,14 +51,20 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
-
+        const winner = calculateWinner(this.state.squares);
+        console.log("Winner: ", winner);
+        let status;
+        if (winner) {
+            status = "Winner: " + winner;
+        } else {
+            status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
+        }
         return (
             <div>
                 <div className="status">{status}</div>
-                {this.renderRow(1, 2, 3)}
-                {this.renderRow(4, 5, 6)}
-                {this.renderRow(7, 8, 9)}
+                {this.renderRow(0, 1, 2)}
+                {this.renderRow(3, 4, 5)}
+                {this.renderRow(6, 7, 8)}
             </div>
         );
     }
@@ -75,6 +84,35 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    let winPositions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winPositions.length; i++) {
+        const [a, b, c] = winPositions[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            console.log("a: ", squares[a]);
+            console.log("b: ", squares[a] === squares[b]);
+            console.log("c: ", squares[a] === squares[c]);
+            console.log(squares[a]);
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
